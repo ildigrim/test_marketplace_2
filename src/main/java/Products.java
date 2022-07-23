bd5fd4bd4c5dc77e811adbdf7d90e88e0b9f0f2f
Eugene Guz <ildigrim.ua@gmail.com>
2022-07-23 15:52:33 +0300

    Changed Products logic according to "Collections".

--- src/main/java/Products.java
+++ src/main/java/Products.java
@@ -1,49 +1,29 @@
-import java.util.Random;
+import java.util.*;
 
 public class Products {
 
-    String[][] products = new String[2][5];
+    private final List<Product> productsList = new ArrayList<>();
 
-    public void addProduct(String product){
-        String[] separated = product.strip().split(" ");
-        Random rand = new Random();
-        if (separated.length == 2) {
-            for (int i = 0; i < 3; i++){
-                if (products[0][i].equals("")){
-                    int getID = rand.nextInt();
-                    products[0][i] = String.valueOf(getID);
-                    products[1][i] = separated[0];
-                    products[2][i] = separated[1];
-                    break;
-                }
-                else {
-                    if (i == 2) {
-                        System.out.println("Can't add a new product. List of products is full.");
-                    }
-                }
-            }
-        }
-        else {
-            System.out.println("Wrong product data format.");
-        }
+    public void addProduct(String productLine){
+        String[] separated = productLine.strip().split(" ");
+        UUID uuid = UUID.randomUUID();
+        Product product = new Product(uuid.toString(), separated[0], Integer.parseInt(separated[1]));
+        productsList.add(product);
     }
 
-    public void deleteProduct(int id){
-        boolean isProductExists = true;
-        for (int i = 0; i < 5; i++) {
-            if (id == Integer.parseInt(products[0][i])){
-                for (int j = 0; j < 2; j++) {
-                    products[j][i] = "";
-                }
+    public boolean deleteProduct(String id){
+        for (Product product: productsList){
+            if (product.getId().equals(id)) {
+                productsList.remove(product);
+                return true;
             }
-            else isProductExists = false;
         }
-        if (!isProductExists) System.out.println("Product with that ID does not exists.");
+        return false;
     }
 
     public void displayAllProducts(){
-        for(int i = 0; i < 5; i++){
-            System.out.println(products[0][i] + " " + products[1][i] + " " + products[2][i]);
+        for (Product product: productsList){
+            System.out.println(product.toString());
         }
     }
 
