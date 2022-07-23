9e64d56e3098484110cc4b9556d46f6821421d95
Eugene Guz <ildigrim.ua@gmail.com>
2022-07-23 13:21:21 +0300

    Adding method for deleting products.

--- src/main/java/Products.java
+++ src/main/java/Products.java
@@ -28,8 +28,17 @@ public class Products {
         }
     }
 
-    public void deleteProduct(){
-
+    public void deleteProduct(int id){
+        boolean isProductExists = true;
+        for (int i = 0; i < 5; i++) {
+            if (id == Integer.parseInt(products[0][i])){
+                for (int j = 0; j < 2; j++) {
+                    products[j][i] = "";
+                }
+            }
+            else isProductExists = false;
+        }
+        if (!isProductExists) System.out.println("Product with that ID does not exists.");
     }
 
 }
