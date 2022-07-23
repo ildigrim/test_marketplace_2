f0eb6ba30935197f8c34ec62fb3c713ba0f388ce
Eugene Guz <ildigrim.ua@gmail.com>
2022-07-23 15:34:55 +0300

    Discowered "Collections". Class Users changed according to them.

--- src/main/java/Users.java
+++ src/main/java/Users.java
@@ -1,50 +1,30 @@
-import java.util.Random;
+import java.util.*;
 
 public class Users {
 
-    String[][] users = new String[3][5];
+    private final List<User> usersList = new ArrayList<>();
 
-    public void addUser(String user){
-        String[] separated = user.strip().split(" ");
-        Random rand = new Random();
-        if (separated.length == 3) {
-            for (int i = 0; i < 4; i++){
-                if (users[0][i].equals("")){
-                    int getID = rand.nextInt();
-                    users[0][i] = String.valueOf(getID);
-                    users[1][i] = separated[0];
-                    users[2][i] = separated[1];
-                    users[3][i] = separated[2];
-                    break;
-                }
-                else {
-                    if (i == 3) {
-                        System.out.println("Can't create a new user. List of user is full.");
-                    }
-                }
-            }
-        }
-        else {
-            System.out.println("Wrong user data format.");
-        }
+    public void addUser(String userLine){
+        String[] separated = userLine.strip().split(" ");
+        UUID uuid = UUID.randomUUID();
+        User user = new User(uuid.toString(), separated[0], separated[1], Integer.parseInt(separated[2]));
+        usersList.add(user);
     }
 
-    public void deleteUser(int id){
-        boolean isUserExists = true;
-        for (int i = 0; i < 5; i++) {
-            if (id == Integer.parseInt(users[0][i])){
-                for (int j = 0; j < 3; j++) {
-                    users[j][i] = "";
-                }
+    public boolean deleteUser(String  id){
+
+        for (User user: usersList){
+            if (user.getId().equals(id)) {
+                usersList.remove(user);
+                return true;
             }
-            else isUserExists = false;
         }
-        if (!isUserExists) System.out.println("User with that ID is not exists.");
+        return false;
     }
 
     public void displayAllUsers(){
-        for(int i = 0; i < 5; i++){
-            System.out.println(users[0][i] + " " + users[1][i] + " " + users[2][i] + " " + users[3][i]);
+        for (User user: usersList){
+            System.out.println(user.toString());
         }
     }
 
