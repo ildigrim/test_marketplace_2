1207916651c2e482cab16035fc360d910b9641b5
Eugene Guz <ildigrim.ua@gmail.com>
2022-07-23 13:02:12 +0300

    Adding method for adding products.

--- src/main/java/Products.java
+++ src/main/java/Products.java
@@ -1,6 +1,31 @@
-public class Products {
-    public void addProduct(){
+import java.util.Random;
 
+public class Products {
+
+    String[][] products = new String[2][5];
+
+    public void addProduct(String product){
+        String[] separated = product.strip().split(" ");
+        Random rand = new Random();
+        if (separated.length == 2) {
+            for (int i = 0; i < 3; i++){
+                if (products[0][i].equals("")){
+                    int getID = rand.nextInt();
+                    products[0][i] = String.valueOf(getID);
+                    products[1][i] = separated[0];
+                    products[2][i] = separated[1];
+                    break;
+                }
+                else {
+                    if (i == 2) {
+                        System.out.println("Can't add a new product. List of products is full.");
+                    }
+                }
+            }
+        }
+        else {
+            System.out.println("Wrong product data format.");
+        }
     }
 
     public void deleteProduct(){
